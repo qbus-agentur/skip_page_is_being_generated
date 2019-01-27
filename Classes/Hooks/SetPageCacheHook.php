c57432c9ff7489a07b83f254a0158d240a6fec05
Benjamin Franzke <bfr@qbus.de>
2019-01-27 18:01:02 +0100

    Add static code analysis using phpstan
    Usage:
      composer install
      .build/vendor/bin/phpstan analyze

--- Classes/Hooks/SetPageCacheHook.php
+++ Classes/Hooks/SetPageCacheHook.php
@@ -14,6 +14,7 @@ class SetPageCacheHook
     /**
      * @param array             $params
      * @param FrontendInterface $frontend
+     * @return void
      */
     public function set(&$params, $frontend)
     {
