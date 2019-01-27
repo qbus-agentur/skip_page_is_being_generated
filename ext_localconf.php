5e9fdb3c3ebc33f9f4bf0827241e756b3cf88936
Benjamin Franzke <bfr@qbus.de>
2019-01-27 17:05:43 +0100

    ext_localconf: Avoid $_EXTKEY usage

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,5 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/cache/frontend/class.t3lib_cache_frontend_variablefrontend.php']['set'][$_EXTKEY] =
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/cache/frontend/class.t3lib_cache_frontend_variablefrontend.php']['set']['skip_page_is_being_generated'] =
         \Qbus\SkipPageIsBeingGenerated\Hooks\SetPageCacheHook::class . '->set';
-
